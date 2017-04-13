379214a1f2fa833d53ac1189489f649f5cfe056d
Erik Uecke <erik.uecke@digitalfilmacademy.edu>
2017-04-13 15:29:44 -0400

    Chapter14 UINavigationController

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -12,7 +12,7 @@ class ItemsViewController: UITableViewController {
     
     var itemStore: ItemStore!
     
-    @IBAction func addNewItem(_ sender: UIButton) {
+    @IBAction func addNewItem(_ sender: UIBarButtonItem) {
         // Creat a new item and add it to the store
         let newItem = itemStore.createItem()
         
@@ -24,23 +24,6 @@ class ItemsViewController: UITableViewController {
             tableView.insertRows(at: [indexPath], with: .automatic)
         }
         
-    }
-    
-    @IBAction func toggleEditingMode(_ sender: UIButton) {
-        // If you are currently in editing mode...
-        if isEditing {
-            // Change text of button to inform user of state
-            sender.setTitle("Edit", for: .normal)
-            
-            // Turn off editing mode
-            setEditing(false, animated: true)
-        } else {
-            // Change text of button to inform user of state
-            sender.setTitle("Done", for: .normal)
-            
-            // Enter editing mode
-            setEditing(true, animated: true)
-        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -77,13 +60,6 @@ class ItemsViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Get the height of the status bar
-        let statusBarHeight = UIApplication.shared.statusBarFrame.height
-        
-        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
-        tableView.contentInset = insets
-        tableView.scrollIndicatorInsets = insets
         
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 65
@@ -155,6 +131,18 @@ class ItemsViewController: UITableViewController {
         }
     }
     
+    override func viewWillAppear(_ anitmated: Bool) {
+        super.viewWillAppear(anitmated)
+        
+        tableView.reloadData()
+    }
+    
+    // Left bar button item
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        navigationItem.leftBarButtonItem = editButtonItem
+    }
 }
 
 
