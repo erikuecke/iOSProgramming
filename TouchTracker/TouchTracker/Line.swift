88414ea008aa5f02b209240c7ee30181698e543c
Erik Uecke <erik.uecke@digitalfilmacademy.edu>
2017-04-19 15:45:16 -0400

    Chapter18 Touch Events & UIResponder

---
+++ TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Erik Uecke on 4/19/17.
+//  Copyright © 2017 Erik Uecke. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
